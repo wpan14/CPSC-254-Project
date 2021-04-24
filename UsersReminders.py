67c59537df3b2e2d21e6177625dd5468eb8ac2c2
ClayKG <43522247+ClayKG@users.noreply.github.com>
2021-04-24 15:31:58 -0700

    Added Comments and fixed sql datetime
    added comments
    SQL now using local time instead of UTC when determining if a reminder is old

--- UsersReminders.py
+++ UsersReminders.py
@@ -4,18 +4,9 @@ import sqlite3
 
 class UsersReminders:
     def __init__(self, db_name): #username
-        # dates = {
-        #     0 : monhours[], #monday
-        #     1 : tuehours[], #tuesday
-        #     2 : wedhours[], #wednesday
-        #     3 : thuhours[], #thursday
-        #     4 : frihours[], #friday
-        #     5 : sathours[], #saturday
-        #     6 : sunhours[] #sunday
-        # }
-        self.db = sqlite3.connect(db_name)
+        self.db = sqlite3.connect(db_name) # connect to databse.  if database with name "db_name" does not exist, it will be created
         self.cur = self.db.cursor()
-        self.cur.execute("CREATE TABLE if not exists Reminders (User text, Day date, Hour text, Minute text, Message text)")
+        self.cur.execute("CREATE TABLE if not exists Reminders (User text, Day date, Hour text, Minute text, Message text)") # create reminders table if it doesn't already exist
         print("========= Initial data in database on startup =========")
         for row in self.cur.execute('SELECT * FROM Reminders'):
             # our reminder fields
@@ -24,54 +15,45 @@ class UsersReminders:
             RmHour = row[2]
             RmMinute = row[3]
             RmMessage = row[4]
-
             print(RmUser +','+ RmDay +','+ RmHour +','+ RmMinute +','+ RmMessage)
 
+        print()
         print("=========== Deleting old reminder records ============")
-        self.cur.execute("DELETE FROM Reminders WHERE datetime(Day,'+' || Hour || ' hours','+' || Minute ||' minutes') < datetime()")
+        self.cur.execute("DELETE FROM Reminders WHERE datetime(Day,'+' || Hour || ' hours','+' || Minute ||' minutes') < datetime('now', 'localtime')")
         self.db.commit()    
-
-        #self.cur.execute("DELETE FROM Reminders;")      # Delete to save previous results
-        #self.dates = dict.fromkeys(range(7), [])
-        #   self.messageauthor = messageauthor
-        #   print(self.dates)
-
-    def __del__(self):
-        self.db.commit()
-        self.db.close()
-
-    def add(self, author, day, hour, minute, msg):
-        #self.dates[day].append([datetime.time(hour, minute), msg])
-
-        #==== On New Reminder ======================================================================================================================================
-        #cur = con.cursor()
-
-        ReminderRecord = (str(author), day, hour, minute, msg)
-
-        self.cur.execute('INSERT INTO Reminders VALUES (?,?,?,?,?)', ReminderRecord)
-        self.db.commit()
-
-        for row in self.cur.execute('SELECT * FROM Reminders'):
+        #display remaining records
+        for row in self.cur.execute("SELECT * FROM Reminders WHERE datetime(Day,'+' || Hour || ' hours','+' || Minute ||' minutes') < datetime()"):
             # our reminder fields
             RmUser = row[0]
             RmDay = row[1]
             RmHour = row[2]
             RmMinute = row[3]
             RmMessage = row[4]
-
             print(RmUser +','+ RmDay +','+ RmHour +','+ RmMinute +','+ RmMessage)
 
-        #display all current reminders to console.  remove this later
-        print()
 
-        #===========================================================================================================================================================
-    def getReminders(self, day, hour, minute):
+
+    def __del__(self):
+        self.db.commit()
+        self.db.close()
+
+    # adds new reminder to database
+    def add(self, author, day, hour, minute, msg):
+        ReminderRecord = (str(author), day, hour, minute, msg)
+
+        self.cur.execute('INSERT INTO Reminders VALUES (?,?,?,?,?)', ReminderRecord) # insert into reminder table
+        self.db.commit()
+        print("reminder added: "+str(author) +','+ str(day) +','+ str(hour) +','+ str(minute) +','+ msg)
+
+    # returns all reminders with specified date and time
+    def getReminders(self, day, hour, minute): #(string,string,string) return list (of table rows)
         tmpList = []
         tmp =  self.cur.execute('SELECT * FROM Reminders WHERE Day=? AND Hour=? AND Minute=?;', (day, hour, minute))
         for x in tmp:
             tmpList.append(x)
         return tmpList
 
-    def deleteReminder(self, user, day, hour, minute, message):
+    # deletes reminder with specified user, day, time, and message
+    def deleteReminder(self, user, day, hour, minute, message): #(string,string,string,string,string) return nothing
         self.cur.execute('DELETE FROM Reminders WHERE User=? AND Day=? AND Hour=? AND Minute=? AND Message=?;', (user, day, hour, minute, message))
         self.db.commit()
--- bot.py
+++ bot.py
@@ -7,17 +7,17 @@ import asyncio
 from UsersReminders import UsersReminders
 from discord.ext import commands
 
-#client = discord.Client()
-commandSymbol = '!'
+commandSymbol = '!' # symbol to denote a call to the bot
 bot = commands.Bot(command_prefix='!')
 default_channel = 812184884218953743#server-id = 812184884218953738
 index = 0
 
+# iniialize reminders database
 reminders = UsersReminders('DiscordReminders.db')
 
+# Checks reminders stored in database every minute to determine if there is one that must be displayed
 async def time_check():
     await bot.wait_until_ready()
-    #print("working")
     while True:
         now = datetime.datetime.now()
         reminderDate = now + timedelta(minutes=1)
@@ -26,19 +26,21 @@ async def time_check():
         await asyncio.sleep(totalSeconds)
 
         now = datetime.datetime.now()
-        for row in reminders.getReminders(datetime.date.today(), str(now.hour), str(now.minute)):
+        for row in reminders.getReminders(datetime.date.today(), str(now.hour), str(now.minute)): # loops though all reminders with a datetime now
             print(row)
-            await bot.get_channel(default_channel).send("[%s] %s" % (row[0], row[4]))
-            #delete reminder after reminder is sent
-            reminders.deleteReminder(row[0], row[1], row[2], row[3], row[4])
+            await bot.get_channel(default_channel).send("[%s] %s" % (row[0], row[4])) # sends reminder in discord
+            
+            reminders.deleteReminder(row[0], row[1], row[2], row[3], row[4]) #delete reminder after reminder is sent
 
-def day(date): #(string) return date
-    day = date[date.find(' ') + 1: date.find(' ', 5)]  #!add Saturday 15:03 test1  MM/DD/YYYY
+# parses date/day of reminder out of the message to the bot.  returns date of the reminder
+def day(msg): #(string) return date
+    day = msg[msg.find(' ') + 1: msg.find(' ', 5)] #finds substring that should be day/date
     day = day.upper()
 
     today = datetime.date.today()
-    currentWeekDay = datetime.date.weekday(today) #int monday = 0
+    currentWeekDay = datetime.date.weekday(today) 
 
+    # if weekday was found instead of a date, return the date of the next found weekday
     if day == "MONDAY":
         return nextWeekDayDate(currentWeekDay,0)
     if day == "TUESDAY":
@@ -55,19 +57,20 @@ def day(date): #(string) return date
         return nextWeekDayDate(currentWeekDay,6)
     
     try:
-        targetDate = datetime.datetime.strptime(day, '%m-%d-%Y').date()
+        targetDate = datetime.datetime.strptime(day, '%m-%d-%Y').date() # checks for date formatted like month-day-year
         return targetDate
-    except:
+    except: # if month-day-year not found
         try:
-            targetDate = datetime.datetime.strptime(day, '%m/%d/%Y').date()
+            targetDate = datetime.datetime.strptime(day, '%m/%d/%Y').date() # checks for date formatted like month/day/year
             return targetDate
-        except:
-            return -1
+        except: # if month/day/year not found
+            return -1 # this means no valid weekday or date was found.
 
-        return -1
+        return -2 # you should never see this
 
     return -2 # you should never see this
 
+# returns next date of weekday
 def nextWeekDayDate(currentWeekDay, targetWeekDay): #(int,int) return date
     if currentWeekDay > targetWeekDay:
         return (datetime.date.today() + timedelta(days=(7-(currentWeekDay-targetWeekDay))))
@@ -76,9 +79,9 @@ def nextWeekDayDate(currentWeekDay, targetWeekDay): #(int,int) return date
     elif currentWeekDay == targetWeekDay:
         return (datetime.date.today())
 
-def hour(date):
-    print(date[date.find(' ', 5) + 1 : ])
-    hour = date[date.find(' ', 5) + 1 : date.find(':')]
+# parses hour of reminder out of the message to the bot.  returns int of the hour
+def hour(msg): #(string) return int
+    hour = msg[msg.find(' ', 5) + 1 : msg.find(':')] # finds substring that should be hour
     hour = int(hour)
     if hour > 23:
         hour = 23
@@ -88,8 +91,9 @@ def hour(date):
 
     return hour
 
-def minute(date):
-    minute = date[date.find(':') + 1 : date.find(' ', date.find(':'))]
+# parses minute of reminder out of the message to the bot.  returns int of the minute
+def minute(msg): #(string) return int
+    minute = msg[msg.find(':') + 1 : msg.find(' ', msg.find(':'))] # finds substring that should be minute
     minute = int(minute)
 
     if minute > 59:
@@ -100,9 +104,9 @@ def minute(date):
 
     return minute
 
-def msgContent(msg):
-    tmpMsg = msg[msg.find(' ', msg.find(':')) + 1:]
-    print(tmpMsg)
+# parses reminder text of reminder out of the message to the bot.  returns string of the reminder text
+def msgContent(msg): #(string) return string
+    tmpMsg = msg[msg.find(' ', msg.find(':')) + 1:] # finds substring that should be reminder text (everything left over after the date/day and time)
     return tmpMsg
 
 # thread = threading.Thread(target = check_time, args = ())
@@ -112,7 +116,7 @@ def msgContent(msg):
 async def on_ready():
     print('Bot is ready.')
 
-
+#=== links to music playlists on YouTube ===================================================================================================
 @bot.command(name ='jazz')
 async def jazz(ctx):  # link to 24/7 livestream of jazz music
     await ctx.send('https://www.youtube.com/watch?v=Dx5qFachd3A')
@@ -136,25 +140,18 @@ async def classical(ctx):
 @bot.command(name ='funk')  # link to 24/7 livestream of funk music
 async def funk(ctx):
     await ctx.send('https://www.youtube.com/watch?v=L1vXBMmH-Fw')
-
+#===========================================================================================================================================
 
 
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
+    if message.author == bot.user: # prevents bot from ever calling itself
         return
-        '''
-    if message.content.startswith(commandSymbol + "channelid"):
-        default_channel = int(message.content[11: len(message.content)]) #client.get_channel()
-        print(message.content[11: len(message.content)])
-        await message.channel.send('Channel set to: ' + str(default_channel))'''
-    
-
-    if message.content.startswith(commandSymbol + 'add'):
-        if (day(message.content) == -1):
+    if message.content.startswith(commandSymbol + 'add'): # if user calls the add function to add a new reminder
+        if (day(message.content) == -1): # invalid day/date
             await message.channel.send("Invalid day, please try again\n $add day hour(24hr):minute")
-        else:
+        else: # add the reminder
             reminders.add(message.author.mention, day(message.content), hour(message.content), minute(message.content), msgContent(message.content))
             await message.channel.send("{} reminder added".format(message.author.mention))
     else:
